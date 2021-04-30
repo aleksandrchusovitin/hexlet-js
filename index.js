8e30b723ebae9f4a9ccb7931e9029f4aedd84847
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-04-30 21:53:56 +0700

    add prettier

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import _ from "lodash";
 
-console.log('Hello, Hexlet!');
-console.log(_.last(['one', 'two']));
+console.log("Hello, Hexlet!");
+console.log(_.last(["one", "two"]));
+
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
